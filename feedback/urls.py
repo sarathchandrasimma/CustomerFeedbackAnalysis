ff04acce9194e94fe562a1726b8281c5e0ae59c4
sarathchandrasimma <sarathchandrarocking@gmail.com>
2024-07-28 00:25:41 +0530

    updated views and urls

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import index, home, about, contact
 
 urlpatterns = [
-    # path('', home, name='home'),
-    # path('about/', about, name='about'),
-    # path('contact/', contact, name='contact'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
     path('feedback/', index, name='index'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -134,11 +134,11 @@ def index(request):
         form = FeedbackForm()
     return render(request, 'index.html', {'form': form})
 
-# def home(request):
-#     return render(request, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
-# def about(request):
-#     return render(request, 'about.html')
+def about(request):
+    return render(request, 'about.html')
 
-# def contact(request):
-#     return render(request, 'contact.html')
+def contact(request):
+    return render(request, 'contact.html')
